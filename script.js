0dafd70b6750aff947cb696c2f0545b77ca01538
Nadezhda Vilboa <77847999+nadyavilboa@users.noreply.github.com>
2023-12-01 23:49:53 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.how-to-use');
+const btnListOpen = container.querySelector('.how-to-use__btn');
+
+if (container && btnListOpen) {
+  function toggleList () {
+    container.classList.toggle('how-to-use--open');
+    
+    if (container.classList.contains('how-to-use--open')) {
+      gsap.to(".how-to-use__down", {
+        height: auto,
+      })
+      
+      return 0;
+    }
+    
+      gsap.to(".how-to-use__down", {
+        height: 0,
+      })
+  }
+  
+  btnListOpen.addEventListener('click', toggleList);
+}
